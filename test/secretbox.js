80125bca6d6fbdf6ceb8aebd5aa40f00dadfd97c
Kyle Maas <kylemaasdev@gmail.com>
2021-01-15 08:32:07 -0500

    Bump version of chloride to 2.4.0

--- test/secretbox.js
+++ test/secretbox.js
@@ -2,7 +2,9 @@ var tape = require("tape");
 var ssbkeys = require("..");
 
 tape("secretBox, secretUnbox", function (t) {
-  var key = Buffer.from("somewhere-over-the-rainbow-way-up-high");
+  var key = Buffer.from(
+    "somewhere-over-the-rainbow-way-up-high".substring(0, 32)
+  );
 
   var boxed = ssbkeys.secretBox({ okay: true }, key);
   if (process.env.VERBOSE_TESTS) console.log("boxed", boxed);
